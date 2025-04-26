ad358631e2f06400ff6e485b1e4f925b6cdea2b9
Stephen D <webmaster@scd31.com>
2025-04-26 16:41:36 -0300

    comfyui-frontend-package: init at 1.17.11

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -2729,6 +2729,8 @@ self: super: with self; {
 
   cometblue-lite = callPackage ../development/python-modules/cometblue-lite { };
 
+  comfyui-frontend-package = callPackage ../development/python-modules/comfyui-frontend-package { };
+
   comicapi = callPackage ../development/python-modules/comicapi { };
 
   comicon = callPackage ../development/python-modules/comicon { };
